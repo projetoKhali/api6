4688ff51749fa31d5da1ba2a8194fa83b1a735ae
JhoantanLop <jhooliveira.lopes@gmail.com>
2025-03-10 16:35:10 -0300

    feat: implement CRUD examples operation in mongo_initialize.py

--- apps/db/mongo_initialize.py
+++ apps/db/mongo_initialize.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 
-def mongo_connect(database_name, url="mongodb://admin:password@localhost:27017/"): # para encontrar o IP de conexão do MongoDB, execute o comando 'docker inspect mongodb'
+def mongo_connect(database_name, url="mongodb://admin:password@localhost:27017/"):
     try:
         client = MongoClient(url)
         db = client[database_name]
@@ -20,7 +20,7 @@ def create_collections(db):
         },
         "plots": {
             "area": 0.0,
-            "coordinates": [0.0, 0.0], # [latitude, longitude]
+            "coordinates": [0.0, 0.0],
             "city": "",
             "state": "",
             "country": ""
@@ -43,13 +43,42 @@ def create_collections(db):
     }
     
     for collection, structure in collections.items():
-        if collection not in db.list_collection_names():
+        if (collection not in db.list_collection_names()):
             db.create_collection(collection)
             db[collection].insert_one(structure)
             print(f"Coleção '{collection}' criada com estrutura padrão.")
         else:
             print(f"Coleção '{collection}' já existe.")
 
+def crud_operations(db):
+    species_collection = db["species"]
+    
+    # Create
+    new_species = {
+        "scientific_name": "Solanum lycopersicum",
+        "common_name": "Tomato",
+        "growth_time": "60-80 days",
+        "water_requirement": "Moderate"
+    }
+    species_id = species_collection.insert_one(new_species).inserted_id
+    print(f"Nova espécie inserida com ID: {species_id}")
+
+    # Read
+    species = species_collection.find_one({"_id": species_id})
+    print(f"Espécie encontrada: {species}")
+
+    # Update
+    updated_species = species_collection.update_one(
+        {"_id": species_id},
+        {"$set": {"common_name": "Tomato (Updated)"}}
+    )
+    print(f"Espécies atualizadas: {updated_species.modified_count}")
+
+    # Delete
+    deleted_species = species_collection.delete_one({"_id": species_id})
+    print(f"Espécies deletadas: {deleted_species.deleted_count}")
+
 if __name__ == "__main__":
     db = mongo_connect("Agrodb")
     create_collections(db)
+    crud_operations(db)
