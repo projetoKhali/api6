37b6560cee8894d7a2a46cf108f4f4f6a517e2a4
Nicole Souza <nicolemadeleines@gmail.com>
2025-04-26 16:26:06 -0300

    feat. Adding the login route to the app

--- apps/web/src/app/app.tsx
+++ apps/web/src/app/app.tsx
@@ -10,33 +10,37 @@ import EventsRegister from '../pages/EventsRegister';
 import '../styles.css';
 import YieldRegister from '../pages/YieldRegister';
 import ProjectionPage from '../pages/ProjectionPage';
+import Login from '../pages/Login';
 
 function App() {
   return (
     <Router>
       <div className="main-container">
-        <div
-          style={{
-            width: '100%',
-            backgroundColor: '#026734',
-          }}
-        >
-          <Navbar />
-        </div>
-        <div style={{ height: '100%', width: '100%' }}>
-          <Routes>
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/projection" element={<ProjectionPage />} />
-            <Route
-              path="/register"
-              element={<Navigate to="/register/yield" replace />}
-            />
-            <Route path="/register">
-              <Route path="yield" element={<YieldRegister />} />
-              <Route path="event" element={<EventsRegister />} />
-            </Route>
-          </Routes>
-        </div>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+
+          <Route path="*" element={
+            <>
+              <div style={{ width: '100%', backgroundColor: '#026734' }}>
+                <Navbar />
+              </div>
+              <div style={{ height: '100%', width: '100%' }}>
+                <Routes>
+                  <Route path="/dashboard" element={<Dashboard />} />
+                  <Route path="/projection" element={<ProjectionPage />} />
+                  <Route
+                    path="/register"
+                    element={<Navigate to="/register/yield" replace />}
+                  />
+                  <Route path="/register">
+                    <Route path="yield" element={<YieldRegister />} />
+                    <Route path="event" element={<EventsRegister />} />
+                  </Route>
+                </Routes>
+              </div>
+            </>
+          } />
+        </Routes>
       </div>
     </Router>
   );
