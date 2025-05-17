ca0db7b91f5cee4f17ab9820546f085c2b1b01a4
Nicole Souza <nicolemadeleines@gmail.com>
2025-05-16 21:47:47 -0300

    feat. adding the logout button

---
+++ apps/web/src/components/LogoutButton.tsx
@@ -0,0 +1,24 @@
+// LogoutButton.tsx
+import { useNavigate } from 'react-router-dom';
+import { logout } from '../service/AuthService';
+
+
+interface Props {
+    setIsAuthenticated: (auth: boolean) => void;
+}
+
+export const LogoutButton = ({ setIsAuthenticated }: Props) => {
+    const navigate = useNavigate();
+
+    const handleLogout = async () => {
+        try {
+            await logout();
+            setIsAuthenticated(false);
+            navigate('/login');
+        } catch (error) {
+            console.error('Erro ao fazer logout:', error);
+        }
+    };
+
+    return <button onClick={handleLogout}>Sair</button>;
+};
