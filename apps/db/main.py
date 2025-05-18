2a82c1a454195f2935f79262efc7f6028f19f602
JhoantanLop <jhooliveira.lopes@gmail.com>
2025-05-18 15:34:09 -0300

    feat(db): issue 78, create database for keys to encrypt and decript user informations

--- apps/db/main.py
+++ apps/db/main.py
@@ -1,7 +1,9 @@
 from db.mongo import initialize_mongo_database
 from db.postgres import initialize_postgres_database
+from db.pg_keys import initialize_key_database
 
 
 def initialize_databases():
     initialize_postgres_database()
     initialize_mongo_database()
+    initialize_key_database()
