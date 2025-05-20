db10cf2ce4872df008a2785b7f21f81520f523a3
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2025-05-20 20:09:59 -0300

    chore(db): use subcommands instead of `main.py` for `init` nx command

--- apps/db/main.py
+++
@@ -1,9 +0,0 @@
-from db.mongo import initialize_mongo_database
-from db.postgres import initialize_postgres_database
-from db.pg_keys import initialize_keys_database
-
-
-def initialize_databases():
-    initialize_postgres_database()
-    initialize_mongo_database()
-    initialize_keys_database()
